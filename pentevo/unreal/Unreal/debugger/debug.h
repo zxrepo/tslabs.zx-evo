4c16d513eb9919b97b84434482359d0d8cc16f0a
Дмитрий <demyanenko.dmitry@gmail.com>
2019-01-09 11:22:53 +0200

    refactoring 5 of 1145

--- pentevo/unreal/Unreal/debugger/debug.h
+++ pentevo/unreal/Unreal/debugger/debug.h
@@ -12,20 +12,16 @@ typedef struct
 extern unsigned dbg_extport;
 extern u8 dgb_extval; // extended memory port like 1FFD or DFFD
 
-extern unsigned mem_sz;
 extern unsigned mem_disk;
 extern unsigned mem_track;
 extern unsigned mem_max;
 
-extern u8 mem_dump;
 extern unsigned show_scrshot;
-extern u8 mem_ascii;
 
 
 extern unsigned ripper; // ripper mode (none/read/write)
 
 extern unsigned user_watches[3];
-extern unsigned regs_curs;
 
 extern u8 trace_labels;
 
