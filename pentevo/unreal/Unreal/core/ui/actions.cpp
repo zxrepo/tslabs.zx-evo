54ac2f97cb9fc7ce8eef0261dc838ed212561a2e
Дмитрий <demyanenko.dmitry@gmail.com>
2019-01-22 11:21:36 +0200

    refactoring 29 of 1145

---
+++ pentevo/unreal/Unreal/core/ui/actions.cpp
@@ -0,0 +1,20 @@
+#include "std.h"
+#include "actions.h"
+
+std::vector<UIAction<>*> actions_registry = std::vector<UIAction<>*>();
+Actions actions{};
+
+auto get_actions(const std::vector<ActionType>& types) -> std::vector<UIAction<>*>
+{
+	auto result = std::vector<UIAction<>*>();
+
+	for (auto type : types)
+		for (auto item : actions_registry)
+		{
+			if (item->type == type)
+				result.push_back(item);
+		}
+
+	return result;
+}
+
